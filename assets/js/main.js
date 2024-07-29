a0afc847bb731d5c50cc6e4eaef53b34dd7d45d2
Maxohl <maxohl12@gmail.com>
2024-07-29 10:35:56 -0300

    added portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,6 +37,18 @@ function updateLanguages(profileData){
 
 }
 
+function updatePortfolio(profileData){
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+            <li>
+                <h3 ${project.github ? 'class="github"' : ''}> ${project.name} </h3>
+                <a href="${project.url}" target="_blank">${project.url}</a>
+            </li>
+        `
+    }).join('')
+}
+
 (async () => {
     const profileData = await fetchProfileData()
     updateProfile(profileData)
