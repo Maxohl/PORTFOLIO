4e4fa7937720cec1888894de955397638a8fbca5
Maxohl <maxohl12@gmail.com>
2024-07-29 09:25:44 -0300

    Added img folder for data

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,9 +18,16 @@ function updateProfile(profileData){
     
     const email = document.getElementById('profile.email')
     email.innerText = profileData.email
+    email.href = `mailto:${profileData.email}` 
+}
+
+function updateSoftSkills(profileData){
+    const softSkills =  document.getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
 }
 
 (async () => {
     const profileData = await fetchProfileData()
     updateProfile(profileData)
+    updateSoftSkills(profileData)
 })()
